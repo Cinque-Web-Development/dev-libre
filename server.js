4fef0f105f3719ba216fa1c55f5fe1144c45d867
Samuel Trahan <samtrahan91@gmail.com>
2020-12-31 15:23:55 -0600

    youtube selected video and styling

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const key = process.env.GOOGLE_KEY
 
 
 app.get("/videos",(req , res) => {
-  console.log(req.query.youtuber)
   axios.get(`https://www.googleapis.com/youtube/v3/search/?part=snippet&type=video&maxResults=5&q=${req.query.youtuber}&key=${key}`)
   .then((response) => {
-    console.log(response.data)
     res.send(response.data)
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err.response.status, err.response.data, err.response.headers))
 })
 
 app.listen(3001, () => console.log('Dev app listening on port 3001!'));
--- src/components/YouTube/YouTubeVideos.js
+++ src/components/YouTube/YouTubeVideos.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function YouTubeVideos({video}) {
+export default function YouTubeVideos({video, onVideoSelect}) {
   return (
-    <div>
-      <img alt={video.snippet.title} src={video.snippet.thumbnails.high.url}></img>
+    <div className="youtube-video" onClick={() => onVideoSelect(video)}>
+      <img className="youtube-img" alt={video.snippet.title} src={video.snippet.thumbnails.high.url}></img>
     </div>
   )
 }
