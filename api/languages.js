c6c0ccd37bfb5fb82cfa0f6b64ea64856972d0f0
Samuel Trahan <samtrahan91@gmail.com>
2021-01-11 13:22:20 -0600

    api key fix in the languagesfile

--- api/languages.js
+++ api/languages.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-const key = process.env.API_KEY;
+const key = process.env.GOOGLE_KEY;
 
 
 function getLanguageVideos(req, res) {
+    console.log(req.query)
 axios.get(`https://www.googleapis.com/youtube/v3/search/?part=snippet&type=video&maxResults=5&q=${req.query.youtuber}+${req.query.language}&key=${key}`)
 .then((response) => {
   res.send(response.data);
